b6b0c1bfebb4642fb26603bee94e942c0ec0a74c
NIYAMAT ALAM <140369542+Niyamatalam94@users.noreply.github.com>
2026-08-28 22:41:18 +0530

    Add files via upload

---
+++ ad.js
@@ -0,0 +1,30 @@
+{
+    "Maintenance": false,
+    "ForceUpdate": false,
+    "LatestVersion": "3.0",
+    "UpdateUrl": "https://t.me/",
+    "TrialMode": false,
+    "TrialExpiry": "31/12/2050 11:00pm",
+    "AdminUrl": "https://t.me/",
+    "Marquee": "Admin",
+    "Users": [
+        {
+            "username": "Alhaz Uddin",
+            "ID": "aba8e5096cd4f5ae",
+            "Expiry": "21/11/2026",
+            "Banned": false
+        },
+        {
+            "username": "hares Mahmud",
+            "ID": "5d3677544d6176e3",
+            "Expiry": "26/11/2026",
+            "Banned": false
+        },
+        {
+            "username": "Sk Shakil",
+            "ID": "0c64eb4614485643",
+            "Expiry": "27/08/2086",
+            "Banned": false
+        }
+    ]
+}
